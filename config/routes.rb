58d0d52e5dff84a270eb03bcab6622de98433967
rmarinac89 <rmarinac@icloud.com>
2016-01-31 19:13:38 -0500

    partner page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'pages#home'
 
+  get '/partner' => 'pages#partner'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
